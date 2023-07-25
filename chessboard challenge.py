66fdf34e7f989295a3bd177f18bffc2bc92ccc46
HaTruong05 <125408638+HaTruong05@users.noreply.github.com>
2023-07-25 00:01:39 -0400

    add documentation

--- chessboard challenge.py
+++ chessboard challenge.py
@@ -1,3 +1,4 @@
+# Represents the moves the knight can make
 UP_RIGHT = -15
 UP_LEFT = -17
 
@@ -15,16 +16,29 @@ POSS_COLS = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 def solution(src, dest):
+    """Given a starting square and a destination on an 8x8 board, 
+    return the smallest number of moves the knight need to reach 
+    the end 
+
+    Args:
+        src (int): the starting square
+        dest (int): the destination
+
+    Returns:
+        int: the smallest number of moves needed
+    """        
     source_dict = {0: [src]}
 
-    end_row, end_col = __find_pos(dest)
+    end_row, end_col = __find_position(dest)
 
+    # If destination is same as starting square, the knight doesn't have to move
     if src == dest:
         return 0
     
+    # The largest number of moves a knight need to reach any square on an 8x8 baord is 6
     for i in range (6):
         for source in source_dict[i]:
-            start_row, start_col = __find_pos(source)
+            start_row, start_col = __find_position(source)
             poss_moves = __remove_unnecessary_moves(start_row, end_row, start_col, end_col)
             __find_poss_moves(poss_moves, start_row, start_col)
             for move in poss_moves:
@@ -37,6 +51,19 @@ def solution(src, dest):
 
 
 def __remove_unnecessary_moves(start_r, end_r, start_c, end_c):
+    """Remove the non optimal moves for the knight. For example, 
+    if the destination is to the right of the starting square, 
+    the knight shouldn't consider moving to the left
+
+    Args:
+        start_r (int): the row the knight is in
+        end_r (int): the row the knight wants to reach
+        start_c (int): the col the knight is in
+        end_c (int): the col the knight wants to reach
+
+    Returns:
+        list: Contains only the optimal moves
+    """
     poss_moves = [UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT, RIGHT_UP, RIGHT_DOWN, LEFT_UP, LEFT_DOWN]
     if end_c < start_c:
             poss_moves.remove(RIGHT_DOWN)
@@ -53,50 +80,84 @@ def __remove_unnecessary_moves(start_r, end_r, start_c, end_c):
             poss_moves.remove(UP_RIGHT)
     return poss_moves
 
-def __find_poss_moves(poss_moves, start_row, start_col):
+def __find_poss_moves(moves, start_row, start_col):
+    """Remove illegal moves. For example, if the knight starts on
+    the first or second column, meaning the edge of the board is
+    on the left, the knight can't make moves that involve moving
+    2 squares to the left
 
-    if UP_LEFT in poss_moves:
+    Args:
+        moves (list): contains the moves the knight can make if we
+            don't consider the starting position 
+        start_row (int): the starting row
+        start_col (int): the starting column
+    """
+    if UP_LEFT in moves:
         if start_row == 1 or start_row == 2 or start_col == 1:
-            poss_moves.remove(UP_LEFT)
+            moves.remove(UP_LEFT)
     
-    if UP_RIGHT in poss_moves:
+    if UP_RIGHT in moves:
         if start_row == 1 or start_row == 2 or start_col == 8:
-            poss_moves.remove(UP_RIGHT)
+            moves.remove(UP_RIGHT)
 
-    if DOWN_LEFT in poss_moves:
+    if DOWN_LEFT in moves:
         if start_row == 7 or start_row == 8 or start_col == 1:
-            poss_moves.remove(DOWN_LEFT)
+            moves.remove(DOWN_LEFT)
 
-    if DOWN_RIGHT in poss_moves:
+    if DOWN_RIGHT in moves:
         if start_row == 7 or start_row == 8 or start_col == 8:
-            poss_moves.remove(DOWN_RIGHT)
+            moves.remove(DOWN_RIGHT)
     
-    if LEFT_DOWN in poss_moves:
+    if LEFT_DOWN in moves:
         if start_col == 1 or start_col == 2 or start_row == 8:
-            poss_moves.remove(LEFT_DOWN)
+            moves.remove(LEFT_DOWN)
     
-    if LEFT_UP in poss_moves:
+    if LEFT_UP in moves:
         if start_col == 1 or start_col == 2 or start_row == 1:
-            poss_moves.remove(LEFT_UP)
+            moves.remove(LEFT_UP)
     
-    if RIGHT_DOWN in poss_moves:
+    if RIGHT_DOWN in moves:
         if start_col == 7 or start_col == 8 or start_row == 8:
-            poss_moves.remove(RIGHT_DOWN)
+            moves.remove(RIGHT_DOWN)
 
-    if RIGHT_UP in poss_moves:
+    if RIGHT_UP in moves:
         if start_col == 8 or start_col == 8 or start_row == 1:
-            poss_moves.remove(RIGHT_UP)
+            moves.remove(RIGHT_UP)
             
 
-def __find_pos(src):
-    return __find_start_row(src), __find_start_col(src)
+def __find_position(src):
+    """Given a square, find the position of the square
+
+    Args:
+        src (int): the given square
+
+    Returns:
+        tuple: contains the row and the column the square is in
+    """
+    return __find_row(src), __find_col(src)
 
 
-def __find_start_row(src):
+def __find_row(src):
+    """Given a square, find the row of the square
+
+    Args:
+        src (int): the given square
+
+    Returns:
+        int: the row the square is in
+    """
     return POSS_ROWS[src // 8]
 
 
-def __find_start_col(src):
+def __find_col(src):
+    """Given a square, find the column of the square
+
+    Args:
+        src (int): the given square
+
+    Returns:
+        int: the column the square is in
+    """
     return POSS_COLS[src % 8]
 
 
